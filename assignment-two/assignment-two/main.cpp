36e29c50fe017afd58aa83d951af3893962ffe17
Josef Dolezal <pepik.ml@gmail.com>
2016-11-19 10:30:52 +0100

    Create project for assignment 2

---
+++ assignment-two/assignment-two/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assignment-two
+//
+//  Created by Josef Dolezal on 19/11/16.
+//  Copyright © 2016 Josef Dolezal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
